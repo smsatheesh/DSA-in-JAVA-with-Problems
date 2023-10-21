52724b51d002ac58a3fc981f22578f864ebe4462
SATHEESH <smsatheesh2000@gmail.com>
2023-10-21 08:05:10 +0530

    Added order agnostic binary search algorithm added to the community

--- OrderAgnosticBinarySearch/Implementations/OrderAgnosticBinarySearch.java
+++ OrderAgnosticBinarySearch/Implementations/OrderAgnosticBinarySearch.java
@@ -1,43 +1,30 @@
 import java.util.*;
 
-class BinarySearchAlgorithm {
+class OrderAgnosticBinarySearch {
 
 	static int binarySearchIterativeApproach(int[] arr, int sz, int key) {
 		
-		int low = 0, high = sz - 1;
+		int low = 0, high = sz;
+		boolean isAsc = arr[low] < arr[high - 1];
 		
 		while(low <= high) {
 			int mid = low + ((high - low) / 2);
 			if(arr[mid] == key)
 				return mid;
-			else if(key > arr[mid])
-				low = mid + 1;
-			else
-				high = mid - 1;
+			if(isAsc) {
+				if(key > arr[mid])
+					low = mid + 1;
+				else
+					high = mid - 1;
+			} else {
+				if(arr[mid] < key)
+					high = mid - 1;
+				else
+					low = mid + 1;
+			}
 		}
 		
 		return -1;
-	}
-	
-	static int binarySearchRecursiveApproach(int[] arr, int sz, int key, int low, int high) {
-		
-		int mid = low + ((high - low) / 2);
-	
-		if(low == high) {
-			
-			if(arr[mid] == arr[low])
-				return low;
-			else
-				return -1;
-		}else {
-			
-			if(arr[mid] == key)
-				return mid;
-			else if(arr[mid] > key)
-				return binarySearchRecursiveApproach(arr, sz, key, low, mid - 1);
-			else
-				return binarySearchRecursiveApproach(arr, sz, key, mid + 1, high);
-		}
 	}
 	
 	public static void main(String[] args) {
@@ -57,12 +44,6 @@ class BinarySearchAlgorithm {
 		int resFromIterApp = binarySearchIterativeApproach(arr, size, key);
 		if(resFromIterApp != -1)
 			System.out.println("Result from Iterative approach, Element " + key + " present at " + (++resFromIterApp));
-		else
-			System.out.println("Search element " + key + " not present in the list");
-		
-		int resFromRecurApp = binarySearchRecursiveApproach(arr, size, key, 0, size - 1);
-		if(resFromIterApp != -1)
-			System.out.println("Result from Recursive approach, Element " + key + " present at " + (++resFromRecurApp));
 		else
 			System.out.println("Search element " + key + " not present in the list");
 	}
