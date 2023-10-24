1ca3b4c983c61764c11362dc580f753e225b2642
smsatheesh <147731426+smsatheesh@users.noreply.github.com>
2023-10-24 07:46:08 +0530

    Update Flooring.java

--- BinarySearch/Problems/Flooring Problem/Flooring.java
+++ BinarySearch/Problems/Flooring Problem/Flooring.java
@@ -1,36 +1,35 @@
 
 public class Flooring
 {
-    
-    static int printResult(int[] arr, int target) {
+	static int printResult(int[] arr, int target) {
         
-        int low = 0, high = arr.length;
-        
-        if(target > arr[high - 1])
-            return arr[high - 1];
-        else {
-            
-            while(low <= high) {
-                
-                int mid = low + (high - low) / 2;
-                if(arr[mid] > target)
-                    high = mid - 1;
-                else if(arr[mid] < target)
-                    low = mid + 1;
-                else
-                    return arr[mid];
-            }   
-        }
-        
-        return arr[high];
-    }
+	        int low = 0, high = arr.length;
+	        
+	        if(target > arr[high - 1])
+	            return arr[high - 1];
+	        else {
+	            
+	            while(low <= high) {
+	                
+	                int mid = low + (high - low) / 2;
+	                if(arr[mid] > target)
+	                    high = mid - 1;
+	                else if(arr[mid] < target)
+	                    low = mid + 1;
+	                else
+	                    return arr[mid];
+	            }   
+	        }
+	        
+	        return arr[high];
+	}
     
 	public static void main(String[] args) {
-	
-	    int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
-	    int target = 15;
+
+		int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+	    	int target = 15;
 	    
-	    int resp = printResult(arr, target);
-	    System.out.println(resp);
+	    	int resp = printResult(arr, target);
+	    	System.out.println(resp);
 	}
 }
