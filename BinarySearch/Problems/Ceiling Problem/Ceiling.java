e4780705191d9dce181be03e7ae7131aa0c76edc
SATHEESH <smsatheesh2000@gmail.com>
2023-10-24 07:42:08 +0530

    Added DSA binary search problems to the community

---
+++ BinarySearch/Problems/Ceiling Problem/Ceiling.java
@@ -0,0 +1,36 @@
+
+public class Ceiling
+{
+    
+    static int printResult(int[] arr, int target) {
+        
+        int low = 0, high = arr.length;
+        
+        if(target > arr[high - 1])
+            return -1;
+        else {
+            
+            while(low <= high) {
+                
+                int mid = low + (high - low) / 2;
+                if(arr[mid] > target)
+                    high = mid - 1;
+                else if(arr[mid] < target)
+                    low = mid + 1;
+                else
+                    return arr[mid];
+            }   
+        }
+        
+        return arr[low];
+    }
+    
+	public static void main(String[] args) {
+	
+	    int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+	    int target = 19;
+	    
+	    int resp = printResult(arr, target);
+	    System.out.println(resp);
+	}
+}
