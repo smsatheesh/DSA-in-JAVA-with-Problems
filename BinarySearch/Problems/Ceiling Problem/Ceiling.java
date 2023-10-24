735a9f47410879b40f51d70e826dc8f176f1d629
smsatheesh <147731426+smsatheesh@users.noreply.github.com>
2023-10-24 07:45:17 +0530

    Update Ceiling.java

--- BinarySearch/Problems/Ceiling Problem/Ceiling.java
+++ BinarySearch/Problems/Ceiling Problem/Ceiling.java
@@ -1,36 +1,35 @@
 
 public class Ceiling
 {
-    
-    static int printResult(int[] arr, int target) {
+	static int printResult(int[] arr, int target) {
         
-        int low = 0, high = arr.length;
-        
-        if(target > arr[high - 1])
-            return -1;
-        else {
-            
-            while(low <= high) {
-                
-                int mid = low + (high - low) / 2;
-                if(arr[mid] > target)
-                    high = mid - 1;
-                else if(arr[mid] < target)
-                    low = mid + 1;
-                else
-                    return arr[mid];
-            }   
-        }
-        
-        return arr[low];
-    }
-    
+	        int low = 0, high = arr.length;
+	        
+	        if(target > arr[high - 1])
+	            return -1;
+	        else {
+	            
+	            while(low <= high) {
+	                
+	                int mid = low + (high - low) / 2;
+	                if(arr[mid] > target)
+	                    high = mid - 1;
+	                else if(arr[mid] < target)
+	                    low = mid + 1;
+	                else
+	                    return arr[mid];
+	            }   
+	        }
+	        
+	        return arr[low];
+    	}   
+	
 	public static void main(String[] args) {
 	
-	    int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
-	    int target = 19;
+		int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+		int target = 19;
 	    
-	    int resp = printResult(arr, target);
-	    System.out.println(resp);
+	    	int resp = printResult(arr, target);
+	    	System.out.println(resp);
 	}
 }
