dd7185ce909bd95de4c1cbfb1e747e343d257650
smsatheesh <smsatheesh2000@gmail.com>
2024-04-02 19:44:43 +0530

    Latest methods added on BST implementations in JAVA to the commumnity

--- DATA STRUCTURES/Trees/Binary Tree/Implementations/BinaryTree.java
+++ DATA STRUCTURES/Trees/Binary Tree/Implementations/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Node {
 	
 	int data;
@@ -18,8 +20,78 @@ public class Tree {
 		if( root != null ) {
 			
 			inOrder( root.left );
-			System.out.println( root.data + " " );
+			System.out.print( root.data + " " );
 			inOrder( root.right );
+		}
+	}
+	
+	static void postOrder( Node root ) {
+		
+		if( root != null ) {
+			
+			postOrder( root.left );
+			postOrder( root.right );
+			System.out.print( root.data + " " );
+		}
+	}
+	
+	static void preOrder( Node root ) {
+		
+		if( root != null ) {
+			
+			System.out.print( root.data + " " );
+			preOrder( root.left );
+			preOrder( root.right );
+		}
+	}
+	
+	static int maxHeight( Node node ) {
+		
+		if( node == null )
+			return 0;
+		else {
+			
+			int leftHeight = maxHeight( node.left );
+			int rightHeight = maxHeight( node.right );
+			
+			if( leftHeight > rightHeight )
+				return leftHeight + 1;
+			else
+				return rightHeight + 1;
+		}
+	}
+	
+	static void nodeAtKdistance( Node root, int distance ) {
+		
+		if( root == null )
+			return;
+		else if( distance == 0 )
+			System.out.print( root.data + ", " );
+		else {
+			
+			nodeAtKdistance( root.left, distance - 1 );
+			nodeAtKdistance( root.right, distance - 1 );
+		}
+	}
+	
+	static void printLevelOrder( Node root ) {
+		
+		if( root == null )
+			return;
+		else {
+			
+			Queue<Node> queue = new ArrayDeque<Node>();
+			queue.add( root );
+			
+			while( !queue.isEmpty() ) {
+				
+				Node temp = queue.poll();
+				System.out.print( temp.data + " " );
+				if( temp.left != null )
+					queue.add( temp.left );
+				if( temp.right != null )
+					queue.add( temp.right );
+			}
 		}
 	}
 	
@@ -31,6 +103,26 @@ public class Tree {
 		root.left.left = new Node( 1 );
 		root.right.right = new Node( 30 );
 		
+		System.out.print( "In-order traversal -> " );
 		inOrder( root );
+		System.out.println();
+
+		System.out.print( "Post-order traversal -> " );
+		postOrder( root );
+		System.out.println();
+		
+		System.out.print( "Pre-order traversal -> " );
+		preOrder( root );
+		System.out.println();
+		
+		System.out.println( "Max height or depth of the tree is " + maxHeight( root ));
+		
+		int distance = 1;
+		System.out.print( "Node at a distance " + distance + " is " );
+		nodeAtKdistance( root, distance );
+		System.out.println();
+		
+		System.out.print( "Level-Order Traversal -> " );
+		printLevelOrder( root );
 	}
 }
